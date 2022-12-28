92acd7e3e3e0ada16a5d7804f54055f0af04e646
Amogh Agastya <46256520+amoghagastya@users.noreply.github.com>
2022-12-28 20:03:30 +0530

    Update 2_👎_Limitations.py

--- pages/2_👎_Limitations.py
+++ pages/2_👎_Limitations.py
@@ -15,3 +15,29 @@ st.write('''This is due to how the similar contexts are retrieved. The entire Ch
          topic can be answered accurately.''')
 
 st.write("Please do let me know if you find a solution to this challenge!")
+
+
+st.sidebar.write(f"""
+    ### How it Works
+
+    The bot takes Product titles and Descriptions from the Amazon India Product 
+    Dataset and collates their content into a natural language search and a Conversation tool.
+
+    Ask product related questions like **"Suggest some christmas gift ideas"** or **"What should I buy for
+    my 10 year old brother?"**, or **"I'm looking for organic products"** and it returns relevant results!
+    
+    **Note**: Hit reset chat before asking it a question of a different topic. Check the limitations tab for the current challenges.
+    
+    The app is powered using OpenAI's embedding service with Pinecone's vector database. The whole process consists
+    of *three* steps:
+    
+    **1**. User Queries are fed into OpenAI's embeddings service to generate a {'4096'}-dimensional query vector.
+    
+    **2**. We use Pinecone to identify similar context vectors (previously encoded from the Amazon Dataset).
+
+    **3**. Relevant contexts are passed in a new question to OpenAI's generative model, returning our answer.
+
+    **How do I make something like this?**
+
+    It's easy! Book a [free Discovery Call with me](https://calendly.com/amagastya/20min) and I'll get your bot setup in no-time!
+""")
